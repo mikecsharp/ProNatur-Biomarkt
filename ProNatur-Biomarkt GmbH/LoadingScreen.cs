89807e520d7660a1b5fa3981724f597a9814719a
WIN10DELPHI\Michael Haas <github@mhlinux.de>
2023-11-04 16:26:51 +0100

    add Project

---
+++ ProNatur-Biomarkt GmbH/LoadingScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProNatur_Biomarkt_GmbH
+{
+    public partial class LoadingScreen : Form
+    {
+        private int loadingBarValue;
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            loadingBarTimer.Start();
+        }
+
+        private void loadingBarTimer_Tick(object sender, EventArgs e)
+        {
+            loadingBarValue += 1;
+            lblLoadingProgress.Text = loadingBarValue.ToString() + "%";
+            loadingProgressBar.Value = loadingBarValue;
+            if (loadingBarValue >= loadingProgressBar.Maximum)
+            {
+                loadingBarTimer.Stop();
+            }
+        }
+
+        
+    }
+}
